7782541fab909b46fcd87b40ac427d853de86a79
Rama Alshareef <105730518+Rama774@users.noreply.github.com>
2023-06-12 16:19:17 +0300

    Add files via upload

---
+++ countWords.py
@@ -0,0 +1,42 @@
+from typing import List
+def countWords(txt : str) -> int:
+
+ while True :
+  a=input("write a sentens")
+  if a=="Plant hope in the hearts of others":
+   print(len(a.split()))
+  if a=='Nora is a very ambitious person':
+   print(len(a.split()))
+  if a=='fine, Thank you':
+   print(len(a.split()))
+  if a=='ok':
+   print(len(a.split()))
+countWords(1)
+#===================================================
+from typing import List
+def countWords(txt : str) -> int:
+ txt='Plant hope in the hearts of others'
+ txt='Nora is a very ambitious person'
+ txt='fine, Thank you'
+ txt='ok'
+ print(len(txt.split()))
+countWords(1)
+#====================================================
+from typing import List
+def countWords(txt : str) -> int:
+ a=input(str)
+ print(len(a.split()))
+countWords(1)
+#====================================================
+from typing import List
+def countWords(txt : str) -> int:
+ txt='Plant hope in the hearts of others'
+ print(len(txt.split()))
+ txt='Nora is a very ambitious person'
+ print(len(txt.split()))
+ txt='fine, Thank you'
+ print(len(txt.split()))
+ txt='ok'
+ print(len(txt.split()))
+countWords(1)
+#=====================================================
